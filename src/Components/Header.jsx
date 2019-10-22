579570f1326018574203fb51f0c6369b91077e66
unknown <seb111.penna@gmail.com>
2019-10-22 12:19:11 +0200

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css';
+
+
+const Header = () => {
+return (
+<header>
+    <h1>Scoreboard</h1>
+    <span className="stats">Player : 1</span>
+
+</header>
+
+)
+
+}
+
+
+export default Header
