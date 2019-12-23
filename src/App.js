b3cd31733bfb14e75620dd451fd117389f793169
unknown <seb111.penna@gmail.com>
2019-12-23 17:05:11 +0100

    create store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React, { Component, } from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import * as PlayerActionCreators from './actions/player';
 import './App.css'
 import Header from './Components/Header'
 import Player from './Components/Player'
@@ -7,119 +9,119 @@ import AddPlayerForm from './Components/AddPlayerForm'
 let arrayIndexSup = [];
 let sup = 0;
 class App extends Component {
-  state = {
-    players: [
-      {
-        name: 'Guil',
-        id: 1,
-        score: 0
-      },
-      {
-        name: 'Treasure',
-        id: 2,
-        score: 0
-      },
-      {
-        name: 'Ashley',
-        id: 3,
-        score: 0
-      },
-      {
-        name: 'James',
-        id: 4,
-        score: 0
-      }
-    ]
-  }
-   checkIfTieBefore = () => {
-    arrayIndexSup.forEach((currentIndex, index) => {
-      if (
-        this.state.players[currentIndex] > this.state.players[currentIndex + 1] &&
-        arrayIndexSup.length >= 2
-      ) {
-        console.log(arrayIndexSup, currentIndex);
-        arrayIndexSup.splice(index + 1);
-        console.log(arrayIndexSup);
-      }
-    });
-  };
+//   state = {
+//     players: [
+//       {
+//         name: 'Guil',
+//         id: 1,
+//         score: 0
+//       },
+//       {
+//         name: 'Treasure',
+//         id: 2,
+//         score: 0
+//       },
+//       {
+//         name: 'Ashley',
+//         id: 3,
+//         score: 0
+//       },
+//       {
+//         name: 'James',
+//         id: 4,
+//         score: 0
+//       }
+//     ]
+//   }
+//    checkIfTieBefore = () => {
+//     arrayIndexSup.forEach((currentIndex, index) => {
+//       if (
+//         this.state.players[currentIndex] > this.state.players[currentIndex + 1] &&
+//         arrayIndexSup.length >= 2
+//       ) {
+//         console.log(arrayIndexSup, currentIndex);
+//         arrayIndexSup.splice(index + 1);
+//         console.log(arrayIndexSup);
+//       }
+//     });
+//   };
 
-   handleHighestScoreAndTie = () => {
-     console.log(arrayIndexSup,"arrayofsupbefore",sup)
-    this.state.players.forEach((players, index) => {
-      if (sup < players.score) {
-        sup = players.score;
-        arrayIndexSup.splice(0, arrayIndexSup.length - 1);
+//    handleHighestScoreAndTie = () => {
+//      console.log(arrayIndexSup,"arrayofsupbefore",sup)
+//     this.state.players.forEach((players, index) => {
+//       if (sup < players.score) {
+//         sup = players.score;
+//         arrayIndexSup.splice(0, arrayIndexSup.length - 1);
         
-        arrayIndexSup[0] = index;
-        console.log(arrayIndexSup,"arrayofsup INTO")
-       this.checkIfTieBefore();
-       console.log(sup)
-      } else if (sup === players.score && players.score!==0) {
-        console.log("sup&pscore",sup,players.score)
-        sup = players.score;
-        arrayIndexSup.push(index);
-        console.log(arrayIndexSup,"arrayofsup INTO equal")
-      }
-    })
-  }
-  handleScoreChange = (operation, index,isHighscore) => {
-    if (isHighscore=== true){
-      sup-=1
-    }
-    this.setState(prevState => {
-      const updatedPlayers = [...prevState.players]
-      const updatedPlayer = { ...updatedPlayers[index] }
-      updatedPlayer.score += operation
-      updatedPlayers[index] = updatedPlayer
-      console.log('into handle score', updatedPlayers)
-      return { players: updatedPlayers }
-    })
-  }
-  handleRemovePlayer = id => {
-    this.setState(prevState => {
-      return {
-        players: prevState.players.filter(p =>{ 
-          arrayIndexSup=arrayIndexSup.filter((indexSup)=>{
-            return indexSup !== id-1
-          })
-         return p.id !==id
-          }
-      )
-      }
-    })
-    arrayIndexSup=[]
-    sup=0
+//         arrayIndexSup[0] = index;
+//         console.log(arrayIndexSup,"arrayofsup INTO")
+//        this.checkIfTieBefore();
+//        console.log(sup)
+//       } else if (sup === players.score && players.score!==0) {
+//         console.log("sup&pscore",sup,players.score)
+//         sup = players.score;
+//         arrayIndexSup.push(index);
+//         console.log(arrayIndexSup,"arrayofsup INTO equal")
+//       }
+//     })
+//   }
+//   handleScoreChange = (operation, index,isHighscore) => {
+//     if (isHighscore=== true){
+//       sup-=1
+//     }
+//     this.setState(prevState => {
+//       const updatedPlayers = [...prevState.players]
+//       const updatedPlayer = { ...updatedPlayers[index] }
+//       updatedPlayer.score += operation
+//       updatedPlayers[index] = updatedPlayer
+//       console.log('into handle score', updatedPlayers)
+//       return { players: updatedPlayers }
+//     })
+//   }
+//   handleRemovePlayer = id => {
+//     this.setState(prevState => {
+//       return {
+//         players: prevState.players.filter(p =>{ 
+//           arrayIndexSup=arrayIndexSup.filter((indexSup)=>{
+//             return indexSup !== id-1
+//           })
+//          return p.id !==id
+//           }
+//       )
+//       }
+//     })
+//     arrayIndexSup=[]
+//     sup=0
     
-  }
+//   }
 
-playersRendererWithHighScoreAndTie = () => {
-  return this.state.players.map((player, index) => {
-    if(arrayIndexSup.includes(index)){
-      return <Player
-      key={player.id.toString()}
-      playerName={player.name}
-      id={player.id}
-      handleRemovePlayer={this.handleRemovePlayer}
-      score={player.score}
-      handleScoreChange={this.handleScoreChange}
-      index={index}
-      highScore="is-high-score"
-    ></Player>
-    }else{
-      return <Player
-      key={player.id.toString()}
-      playerName={player.name}
-      id={player.id}
-      handleRemovePlayer={this.handleRemovePlayer}
-      score={player.score}
-      handleScoreChange={this.handleScoreChange}
-      index={index}
-      highScore="is-not-high-score"
-      ></Player>
-    }
-})
-}
+// playersRendererWithHighScoreAndTie = () => {
+//   return this.state.players.map((player, index) => {
+//     if(arrayIndexSup.includes(index)){
+//       return <Player
+//       key={player.id.toString()}
+//       playerName={player.name}
+//       id={player.id}
+//       handleRemovePlayer={this.handleRemovePlayer}
+//       score={player.score}
+//       handleScoreChange={this.handleScoreChange}
+//       index={index}
+//       highScore="is-high-score"
+//     ></Player>
+//     }else{
+//       return <Player
+//       key={player.id.toString()}
+//       playerName={player.name}
+//       id={player.id}
+//       handleRemovePlayer={this.handleRemovePlayer}
+//       score={player.score}
+//       handleScoreChange={this.handleScoreChange}
+//       index={index}
+//       highScore="is-not-high-score"
+//       ></Player>
+//     }
+// })
+// }
   playersRenderer = () => {
     console.log(this.checkIfOnePointHasBeenScored())
     if(this.checkIfOnePointHasBeenScored()===false){
@@ -142,24 +144,28 @@ playersRendererWithHighScoreAndTie = () => {
     return this.playersRendererWithHighScoreAndTie()
   }
   }
-   handleUpdatePlayersState = (submittedName) =>{
-     this.setState(prevState =>{
-       const updatedPlayers = [...prevState.players]
-       console.log(updatedPlayers,"dfsdf")
-        updatedPlayers.push({name:submittedName,id:this.state.players.length+1,score:0})
-       console.log(updatedPlayers,"addp")
+//    handleUpdatePlayersState = (submittedName) =>{
+//      this.setState(prevState =>{
+//        const updatedPlayers = [...prevState.players]
+//        console.log(updatedPlayers,"dfsdf")
+//         updatedPlayers.push({name:submittedName,id:this.state.players.length+1,score:0})
+//        console.log(updatedPlayers,"addp")
        
-       return {players:updatedPlayers}
-     }
-       )
-   }
+//        return {players:updatedPlayers}
+//      }
+//        )
+//    }
 
-   checkIfOnePointHasBeenScored = () =>{
-     return this.state.players.some( (player,index) => {
-       return player.score >0
-     }
-     )}
+//    checkIfOnePointHasBeenScored = () =>{
+     
+//      return this.state.players.some( (player,index) => {
+//        return player.score >0
+//      }
+//      )}
   render() {
+    const addPlaye = bindActionCreators(PlayerActionCreators.addPlayer,this.props.dispatch);
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer,this.props.dispatch)
+    const updatedPlayerScore = bindActionCreators(PlayerActionCreators.updatedPlayerScore,this.props.dispatch)
     return (
       <div>
         <Header title='scoreboard' players={this.state.players}></Header>
@@ -169,4 +175,10 @@ playersRendererWithHighScoreAndTie = () => {
     )
   }
 }
-export default App
+
+const mapStateToProps = state => (
+  {
+    players:state
+  }
+)  
+export default connect(mapStateToProps)(App)
